cf54f64d5f965c7e38b92ee91904dd1fae394b06
Adarshkumar Pavani <adarshkumar.pavani@gmail.com>
2016-12-04 22:03:59 -0800

    Base project upload

---
+++ Assets/Scripts/SignInManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+
+public class SignInManager : MonoBehaviour {
+    // authenticate user:
+    void Start()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            Debug.Log("Login Successful");
+            // handle success or failure
+        });
+    }
+}
